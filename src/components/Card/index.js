2eba9e2fea081de6405859e5624e4198bf5ece40
katredford <katharine.redford@gmail.com>
2022-08-17 22:54:41 -0500

    images now display on page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,9 +4,9 @@ export default function Card(props) {
   return (
     <section className="work-container">
       <h3>{props.title}</h3> <p>{props.date}</p>
-      <img src={`../images/${props.img[0]}`} />
+      <img src={props.img[0]} alt=''  style={{"height" : "200px", "width" : "200px"}}/>
       <p>{props.post_text}</p>
-      <img src={`url(${props.img[1]})`} />
+      <img src={props.img[1]} alt=''  style={{"height" : "200px", "width" : "200px"}}/>
 
     </section>
   );
