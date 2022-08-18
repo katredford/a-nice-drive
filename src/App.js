568fa91bc7fbbe616e8594391c2c7673218ed5cd
katredford <katharine.redford@gmail.com>
2022-08-18 12:19:39 -0500

    add header, start styles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from "react"
 // import Hero from "./components/Hero"
 // import Card from "./components/Card"
 // import data from "./data"
+import Header from "./components/Header"
 import Post from "./components/Post"
 
 export default function App() {
 
   return(
     <div>
+      
       <main>
+       <Header /> 
         <Post />
         <p>whats happening now?</p>
       </main>
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return(
+  <div className="hstyle">
+    <p className="subtitle">who doesn't love</p>
+    <h1 className="tite"> A Nice Drive</h1>
+    </div>
+  )
+}
